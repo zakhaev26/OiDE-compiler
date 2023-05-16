41eb97efaf74d4906c333572ee614cf9ba69f570
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-05-16 13:01:43 +0530

    storing code in dir using fs module

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
+const fs = require('fs');
 const express = require("express");
 const app = express();
-const compiler=require('compilex');
-const prt = 5000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,35 +13,29 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.render("home.ejs")
     console.log("user hit hpg!")
+
 })
 
-app.post('/',(req,res)=>{
-    const axios = require('axios');
-    let code=res.body.code;
-    const options = {
-  method: 'POST',
-  url: 'https://online-code-compiler.p.rapidapi.com/v1/',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '95992756cbmshdd903a59b07377ap1b3e73jsn36c4b8682a30',
-    'X-RapidAPI-Host': 'online-code-compiler.p.rapidapi.com'
-  },
-  data: {
-    language: 'java',
-    version: 'latest',
-    code: code,
-    input: null
-  }
-};
+app.post('/compile',(req,res)=>{
+    const code = req.body.code;
+    console.log(code);
+    try {
+        fs.writeFileSync('file.scala', code);
+        console.log("File has been saved.");
+    } catch (error) {
+        console.log(error);
+    }
+    res.redirect('/eg');
 
-try {
-	const response = axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-res.render('eg.ejs',{code:response.data})
 })
 
+app.get('/eg',(req,res)=>{
+    res.render('eg.ejs');
+})
+
+
+// app.post('/compile',(req,res)=>{
+
+// })
 
 app.listen(3000);
---
+++ file.scala
@@ -0,0 +1,1 @@
+object Hello {     def main(args: Array[String]) = {         println("Hello, world")     } }
