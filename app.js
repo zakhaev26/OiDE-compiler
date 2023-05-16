f8b49f1399f31d0782a862df28b8f965ec503267
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-05-16 19:54:35 +0530

    IDE interface

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const express = require("express");
 const app = express();
-
+let lang;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,30 +18,40 @@ app.get('/', (req, res) => {
 
 app.post('/compile', async (req, res) => {
     // const code = req.body.code;
-    console.log(req.body);
+    // console.log(req.body);
+    let {c,cpp,dart,csharp,python,rust,scala,golang}=req.body;
+    if(c==='') lang='c';
+    if(cpp==='') lang='cpp';
+    if(dart==='') lang='dart';
+    if(csharp==='') lang='csharp';
+    if(python==='') lang='python3';
+    if(rust==='') lang='rust';
+    if(scala==='') lang='scala';
+    if(golang==='') lang='go';
+    console.log(lang);
+    console.log(req.body.code);
     // try {
     //     fs.writeFileSync('file.scala', code);
     //     console.log("File has been saved.");
     // } catch (error) {
     //     console.log(error);
     // }
-    const axios = require('axios');
 
-    // const options = {
-    //     method: 'POST',
-    //     url: 'https://online-code-compiler.p.rapidapi.com/v1/',
-    //     headers: {
-    //         'content-type': 'application/json',
-    //         'X-RapidAPI-Key': '95992756cbmshdd903a59b07377ap1b3e73jsn36c4b8682a30',
-    //         'X-RapidAPI-Host': 'online-code-compiler.p.rapidapi.com'
-    //     },
-    //     data: {
-    //         language: 'cpp',
-    //         version: 'latest',
-    //         code: code,
-    //         input: null
-    //     }
-    // };
+    const options = {
+        method: 'POST',
+        url: 'https://online-code-compiler.p.rapidapi.com/v1/',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '95992756cbmshdd903a59b07377ap1b3e73jsn36c4b8682a30',
+            'X-RapidAPI-Host': 'online-code-compiler.p.rapidapi.com'
+        },
+        data: {
+            language: lang,
+            version: 'latest',
+            // code: code,
+            input: null
+        }
+    };
 
     // try {
     //     const response = await axios.request(options);
@@ -50,8 +60,12 @@ app.post('/compile', async (req, res) => {
     //     console.error(error);
     // }
 
-    res.redirect('/eg');
+    res.redirect('/code-editor');
 
+})
+
+app.get('/code-editor',(req,res)=>{
+    res.render('code-editor.ejs');
 })
 
 app.get('/eg', (req, res) => {
