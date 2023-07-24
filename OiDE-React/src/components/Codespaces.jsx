af631d174e8b9697d51c0ad9f142aef6a77579a4
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-24 16:01:14 +0530

    server and client connection successful

--- OiDE-React/src/components/Codespaces.jsx
+++ OiDE-React/src/components/Codespaces.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { Container, Textarea, Text, SimpleGrid,Box,Button } from '@chakra-ui/react'
+import axios from 'axios';
 const Codespaces = () => {
     const { lang } = useParams();
-    console.log(lang)
     let [value, setValue] = React.useState('')
 
     let handleInputChange = (e) => {
         let inputValue = e.target.value
         setValue(inputValue)
     }
+
+    let [buttonClick,setbuttonClick] = useState(false)
+    useEffect(()=>{
+
+        const fetchData = async()=>{
+
+            try{
+            const data = await axios.post('http://127.0.0.1:5000/api',{
+                code:value
+            })
+            return data
+        }
+        catch(e){
+            console.log(e.message)
+            return null;
+        }
+    }
+
+        console.log(buttonClick)
+        if(buttonClick===true){
+            console.log('Compiling..')
+            fetchData().then((res)=>{
+                console.log(res.data)
+            }).catch(e=>{
+                console.log(e.message)
+            }).finally(()=>{
+                setbuttonClick(false)
+            })
+        }
+        
+    },[buttonClick])
+
+    let OUTPUT__INJECTION__FROM__NODEJS ='NULL'
+
+
     return (
             <div id='gon' style={{ width: '100%'}}>
                 <h1 style={{textAlign:'center'}}>Welcome to {lang} IDE!</h1>
@@ -19,22 +54,23 @@ const Codespaces = () => {
                     <Box style={{marginLeft:'60px'}}>
                         <Textarea
                             onChange={handleInputChange}
-                            placeholder='Here is a sample placeholder'
+                            placeholder='Input'
                             size='lg'
                             width='90%'
                             height='80%'
-                            
+                            value={value}
                         ></Textarea>
-                <Button style={{marginLeft:'77%',marginTop:'10px'}}>Compile</Button>
+                <Button style={{marginLeft:'77%',marginTop:'10px'}} onClick={()=>{setbuttonClick((prevState)=>!prevState)}} isDisabled={buttonClick}>Compile</Button>
 
                     </Box>
                     <Box>
                         <Textarea style={{marginRight:'10px'}}
                             onChange={handleInputChange}
-                            placeholder='Here is a sample placeholder'
+                            placeholder='Output'
                             size='lg'
                             width='90%'
                             height='80%'
+                            value={OUTPUT__INJECTION__FROM__NODEJS}
                         />
                         
                     </Box>
--- OiDE-server/app.js
+++ OiDE-server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const API = require('./routes/handleapi.js');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/api',API)
 
