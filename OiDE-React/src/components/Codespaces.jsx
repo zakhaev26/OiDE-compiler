dd0c1d61c04321b5912b1cd315bd33fd3a7c0760
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-24 14:46:20 +0530

    Ide interface complete

--- OiDE-React/src/components/Codespaces.jsx
+++ OiDE-React/src/components/Codespaces.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-
+import { Container, Textarea, Text, SimpleGrid,Box,Button } from '@chakra-ui/react'
 const Codespaces = () => {
     const { lang } = useParams();
     console.log(lang)
+    let [value, setValue] = React.useState('')
 
+    let handleInputChange = (e) => {
+        let inputValue = e.target.value
+        setValue(inputValue)
+    }
     return (
-    <div>
-      <h1>Welcome to {lang} IDE!</h1>
-    </div>
-  )
+            <div id='gon' style={{ width: '100%'}}>
+                <h1 style={{textAlign:'center'}}>Welcome to {lang} IDE!</h1>
+                <Text mb='8px'></Text>
+                <SimpleGrid columns={2} spacing={10} height='100vh' style={{scrollbarWidth:'none' ,margin:'0'}}>
+                    
+                    <Box style={{marginLeft:'60px'}}>
+                        <Textarea
+                            onChange={handleInputChange}
+                            placeholder='Here is a sample placeholder'
+                            size='lg'
+                            width='90%'
+                            height='80%'
+                            
+                        ></Textarea>
+                <Button style={{marginLeft:'77%',marginTop:'10px'}}>Compile</Button>
+
+                    </Box>
+                    <Box>
+                        <Textarea style={{marginRight:'10px'}}
+                            onChange={handleInputChange}
+                            placeholder='Here is a sample placeholder'
+                            size='lg'
+                            width='90%'
+                            height='80%'
+                        />
+                        
+                    </Box>
+                </SimpleGrid>
+            </div>
+    )
 }
 
 export default Codespaces
