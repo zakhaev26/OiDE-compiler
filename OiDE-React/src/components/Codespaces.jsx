35d9aaedf5aa69a53af9379c495b5376829e0064
Soubhik Gon <119437069+mogulcoder26@users.noreply.github.com>
2023-07-26 14:10:35 +0530

    Update Codespaces.jsx
    patched extra return statement in react fragment

--- OiDE-React/src/components/Codespaces.jsx
+++ OiDE-React/src/components/Codespaces.jsx
@@ -53,7 +53,7 @@ const Codespaces = () => {
 
 
     return (
-        <div id='gon' style={{ width: '100%' }}>  return (
+        <div id='gon' style={{ width: '100%' }}>
 
             <h1 style={{ textAlign: 'center' }} className='gradient-text'>Welcome to {lang} IDE!</h1>
             <Text mb='8px'></Text>
